e37722ab5fcd9e1cc459cad734a1c100f5516fc0
Tomilov Artem <black_art@bk.ru>
2019-07-21 15:45:40 +0700

    first try

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,124 @@
 package ru.skillbranch.devintensive
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
+import ru.skillbranch.devintensive.models.Bender
+import android.widget.TextView.OnEditorActionListener as OnEditorActionListener1
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //benderImage = findViewById(R.id.iv_bender) as ImageView
+        //benderImage = findViewById<ImageView>(R.id.iv_bender)
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
+
+
+        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
+
+
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        val (r, g, b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+
+        //textTxt.setText(benderObj.askQuestion())
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
+
+
+
+        messageEt.setOnEditorActionListener(TextView.OnEditorActionListener { _, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                hideKeyboard()
+                sendBtn.callOnClick()
+                return@OnEditorActionListener true
+            }
+            false
+        }
+        )
+
+
+
+
+
+        Log.d("M_MainActivity", "onCreate $status $question")
+
+
     }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("M_MainActivity", "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("M_MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("M_MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("M_MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("M_MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("M_MainActivity", "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("STATUS", benderObj.status.name)
+        outState?.putString("QUESTION", benderObj.question.name)
+        Log.d("M_MainActivity", "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name} ")
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send) {
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
+            messageEt.setText("")
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,101 @@
+package ru.skillbranch.devintensive.models
+
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+
+
+    fun askQuestion(): String = when (question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        return if (question.validateAnswer(answer)) {
+            if (question.answers.contains(answer.toLowerCase())) {
+                question = question.nextQuestion()
+                "Отлично - ты справился!\n${question.question}" to status.color
+            } else {
+                if(question == Question.IDLE) return question.question to status.color
+                status = status.nextStatus()
+                if (status == Status.NORMAL) {
+                    question = Question.NAME
+                    "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                } else {
+                    "Это не правильный ответ!\n${question.question}" to status.color
+                }
+            }
+        } else {
+            "${question.validateStr}\n${question.question}" to status.color
+
+
+        }
+    }
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
+    }
+
+    enum class Question(val question: String, val answers: List<String>, val validateStr: String) {
+        NAME("Как меня зовут?", listOf("бендер", "bender"), "Имя должно начинаться с заглавной буквы") {
+            override fun nextQuestion(): Question = PROFESSION
+            override fun validateAnswer(answer: String): Boolean {
+                return !(answer.isEmpty() || answer[0].isLowerCase())
+            }
+        },
+        PROFESSION(
+            "Назови мою профессию?",
+            listOf("сгибальщик", "bender"),
+            "Профессия должна начинаться со строчной буквы"
+        ) {
+            override fun nextQuestion(): Question = MATERIAL
+            override fun validateAnswer(answer: String): Boolean {
+                return !(answer.isEmpty() || answer[0].isUpperCase())
+            }
+        },
+        MATERIAL(
+            "Из чего я сделан?",
+            listOf("металл", "дерево", "metal", "iron", "wood"),
+            "Материал не должен содержать цифр"
+        ) {
+            override fun nextQuestion(): Question = BDAY
+            override fun validateAnswer(answer: String): Boolean {
+                return !answer.contains(Regex("[1-9]+"))
+            }
+        },
+        BDAY("Когда меня создали?", listOf("2993"), "Год моего рождения должен содержать только цифры") {
+            override fun nextQuestion(): Question = SERIAL
+            override fun validateAnswer(answer: String): Boolean {
+                return !answer.contains(Regex("[^0-9]"))
+            }
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057"), "Серийный номер содержит только цифры, и их 7") {
+            override fun nextQuestion(): Question = IDLE
+            override fun validateAnswer(answer: String): Boolean {
+                return !(answer.length != 7 || answer.contains(Regex("[^0-9]")))
+            }
+        },
+        IDLE("На этом все, вопросов больше нет", listOf(), "") {
+            override fun nextQuestion(): Question = IDLE
+            override fun validateAnswer(answer: String): Boolean = true
+        };
+
+        abstract fun nextQuestion(): Question
+        abstract fun validateAnswer(answer: String): Boolean
+    }
+}
